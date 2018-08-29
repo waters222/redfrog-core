a0f6ae8821b56d05be36d9566b9fa87ca6720f01
water <weishi258@gmail.com>
2018-08-29 19:00:13 +0800

    home

--- common/commons.go
+++ common/commons.go
@@ -1,6 +1,5 @@
 package common
 
-
 const (
 	DOMAIN_WHITE_LIST	= false
 	DOMAIN_BLACK_LIST	= true
---
+++ vagrant/run_test_server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+vagrant ssh -c '/home/vagrant/map/src/github.com/weishi258/redfrog-core/vagrant/remote_test.sh'
