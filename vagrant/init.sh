43d15dd3b7ea8210bf39f1eea653c52653c39a28
water <weishi258@gmail.com>
2018-08-30 13:10:21 +0800

    adding two machine in private network for testing purpose

--- vagrant/init.sh
+++ vagrant/init.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-vagrant ssh -c '/home/vagrant/map/src/github.com/weishi258/redfrog-core/vagrant/remote_setup.sh'
+vagrant ssh gateway -c '/home/vagrant/map/src/github.com/weishi258/redfrog-core/vagrant/remote_setup.sh'
+vagrant ssh client -c '/home/vagrant/map/src/github.com/weishi258/redfrog-core/vagrant/remote_setup.sh'
--- vagrant/remote_setup.sh
+++ vagrant/remote_setup.sh
@@ -3,4 +3,7 @@ sudo apt-get update
 sudo apt-get install traceroute -y
 sudo apt-get install golang -y
 sudo apt-get install go-dep -y
-/usr/bin/go get -u github.com/derekparker/delve/cmd/dlv
+export GOPATH=$HOME/go
+git clone https://github.com/derekparker/delve.git $GOPATH/src/github.com/derekparker/delve
+cd $GOPATH/src/github.com/derekparker/delve
+make install
