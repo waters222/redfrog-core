30cc2756b5ed4c999de204cb82790aa79c804cb9
water <weishi258@gmail.com>
2018-10-19 18:51:06 +0800

    fix config

--- start_proxy.sh
+++ start_proxy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-nohup ./redfrog-client -c prod-config.yaml > output.log &
+nohup /home/rock64/redfrog/redfrog-client -c /home/rock64/redfrog/prod-config.yaml > /home/rock64/redfrog/output.log &
 PID=$!
-echo $PID > redfrog.pid
+echo $PID > /home/rock64/redfrog/redfrog.pid
