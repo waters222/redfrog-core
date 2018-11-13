102c12cf1b3775fa35ff873ad21efabe14d44042
water <weishi258@gmail.com>
2018-11-13 16:15:31 +0800

    fix build script for test

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ echo "Building proxy-server"
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags $INJECT_PARAMS -a -o ./bin/redfrog-server ./proxy_server/server.go
 
 echo "Building test"
-env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -a -o ./bin/test ./test/test.go
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o ./bin/test ./test/test.go
 
 echo "Building proxy-client-arm64"
 env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags $INJECT_PARAMS -a -o ./bin/redfrog-client-arm64 ./main/main.go
