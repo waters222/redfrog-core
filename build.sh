99262a33ca3f2511728ad15aa52efb835eaa7ae7
wei.shi <weishi258@gmail.com>
2019-06-27 14:18:39 -0400

    update REAME.d

--- build.sh
+++ build.sh
@@ -14,3 +14,5 @@ env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o ./bin/test ./test/test.
 echo "Building proxy-client-arm64"
 env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags $INJECT_PARAMS -a -o ./bin/redfrog-client-arm64 ./main/main.go
 
+echo "Building proxy-server-arm64"
+env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags $INJECT_PARAMS -a -o ./bin/redfrog-server-arm64 ./proxy_server/server.go
